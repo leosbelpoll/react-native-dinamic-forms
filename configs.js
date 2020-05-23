dfc58d64866de8eb9ca5e6dccbb48e586e4d98e8
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-05-23 18:33:00 -0300

    Config point to local IP

--- configs.js
+++ configs.js
@@ -1,6 +1,4 @@
-// export const API_URL = "http://designing-solutions-smartech.herokuapp.com/api";
-// export const API_URL = "http://192.168.1.7:8000/api";
-export const API_URL = "http://161.35.112.42/api";
+export const API_URL = "http://192.168.1.7:8000/api";
 
 export const ACCESS_TOKEN_IDENTIFIER = "smartech_access_token";
 export const USER_NAME = "smartech_username";
